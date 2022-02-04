881aa4a3807c8fbe83804b9e6b58fbc0c0664e91
Ricardo Camacho Mireles <ricmireles7@gmail.com>
2022-02-04 14:59:07 -0800

    implemented dynamic routing given any fetched tech page

--- src/index.js
+++ src/index.js
@@ -1,10 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "./components/Home";
+import Technology from "./routes/Technology";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
 
+const rootElement = document.getElementById("root");
 ReactDOM.render(
-  <React.StrictMode>
-    <Home />
-  </React.StrictMode>,
-  document.getElementById("root")
+  <BrowserRouter>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/tech/:technology" element={<Technology />} />
+      <Route
+        path="*"
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>Page Not Found: 404</p>
+          </main>
+        }
+      />
+    </Routes>
+  </BrowserRouter>,
+  rootElement
 );
